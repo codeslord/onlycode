6d0c20b57a587c9ce770bbc0583a9f95456c8992
Rohith R Nair <rohithrnair@outlook.com>
2020-09-17 11:19:18 +0530

    Create onlycode.py

---
+++ onlycode.py
@@ -0,0 +1,78 @@
+import os
+
+def get_filepaths(directory):
+    """
+    This function will generate the file names in a directory 
+    tree by walking the tree either top-down or bottom-up. For each 
+    directory in the tree rooted at directory top (including top itself), 
+    it yields a 3-tuple (dirpath, dirnames, filenames).
+    """
+    file_paths = []  # List which will store all of the full filepaths.
+
+    # Walk the tree.
+    for root, directories, files in os.walk(directory):
+        for filename in files:
+            # Join the two strings in order to form the full filepath.
+            filepath = os.path.join(root, filename)
+            file_paths.append(filepath)  # Add it to the list.
+
+    return file_paths  # Self-explanatory.
+
+""" Strip comments and docstrings from a file.
+"""
+
+import sys, token, tokenize
+
+def do_file(fname):
+    """ Run on just one file.
+    """
+    source = open(fname, encoding='utf-8')
+    mod = open(fname + ",strip", "w", encoding='utf-8')
+
+    prev_toktype = token.INDENT
+    first_line = None
+    last_lineno = -1
+    last_col = 0
+
+    tokgen = tokenize.generate_tokens(source.readline)
+    for toktype, ttext, (slineno, scol), (elineno, ecol), ltext in tokgen:
+        if 0:   # Change to if 1 to see the tokens fly by.
+            print("%10s %-14s %-20r %r" % (
+                tokenize.tok_name.get(toktype, toktype),
+                "%d.%d-%d.%d" % (slineno, scol, elineno, ecol),
+                ttext, ltext
+                ))
+        if slineno > last_lineno:
+            last_col = 0
+        if scol > last_col:
+            mod.write(" " * (scol - last_col))
+        if toktype == token.STRING and prev_toktype == token.INDENT:
+            # Docstring
+            mod.write("#--")
+        elif toktype == tokenize.COMMENT:
+            # Comment
+            mod.write("##\n")
+        else:
+            mod.write(ttext)
+        prev_toktype = toktype
+        last_col = ecol
+        last_lineno = elineno
+ 
+ 
+# Run the above function and store its results in a variable.   
+full_file_paths = get_filepaths(r"_path_to_dir_")
+
+for f in tqdm.tqdm(full_file_paths):
+    try:
+        if f.endswith(".py"):
+          do_file(f)
+    except FileNotFoundError:
+        continue
+        
+        
+        
+        
+        
+        
+        
+        
